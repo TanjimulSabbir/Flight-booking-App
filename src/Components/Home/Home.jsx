b1be133e7d604a120f78926180defb6991ac678b
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-13 15:24:34 +0600

    converted-html-to-react-comoponent

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Header from '../Shared/Header'
+import InputedInfo from '../InputedInfo/InputedInfo'
+import DisplayBooking from '../DisplayBooking/DisplayBooking'
+
+const Home = () => {
+  return (
+    <div>
+     <Header></Header>
+     <InputedInfo/>
+     <DisplayBooking/>
+    </div>
+  )
+}
+
+export default Home
